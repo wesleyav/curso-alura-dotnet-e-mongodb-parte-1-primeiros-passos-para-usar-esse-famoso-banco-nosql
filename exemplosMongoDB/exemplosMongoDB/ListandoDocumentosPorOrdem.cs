3cb7b9d3716656d440e538925a0b0d627eeb173a
Wesley <wesley.contato@hotmail.com>
2020-12-10 22:08:28 -0300

    Exibindo resultado da busca por um determinado critério

---
+++ exemplosMongoDB/exemplosMongoDB/ListandoDocumentosPorOrdem.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplosMongoDB
+{
+    class ListandoDocumentosPorOrdem
+    {
+        static void Main(string[] args)
+        {
+            Task T = MainSync(args);
+            Console.WriteLine("Pressione ENTER");
+            Console.ReadLine();
+        }
+
+        static async Task MainSync(string[] args)
+        {
+            var conexaoBiblioteca = new ConectandoMongoDB();
+
+            
+
+            Console.WriteLine("Listando documentos mais de 100 páginas");
+
+            var construtor = Builders<Livro>.Filter;
+            var condicao = construtor.Gt(x => x.Paginas, 100);
+
+            var listaLivros = await conexaoBiblioteca.Livros.Find(condicao).SortBy(x => x.Titulo).Limit(5).ToListAsync();
+
+            foreach (var doc in listaLivros)
+            {
+                Console.WriteLine(doc.ToJson<Livro>());
+
+            }
+
+            Console.WriteLine("Fim da lista!");
+            Console.WriteLine("----------");
+
+            
+        }
+    }
+}
